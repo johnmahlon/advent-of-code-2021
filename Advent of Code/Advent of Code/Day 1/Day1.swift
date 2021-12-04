cb582d5cb0f88891aeac08648190784e5fa8a35b
John Peden <john@johnmahlon.me>
2021-12-04 08:20:34 -0600

    Day 1.2

--- Advent of Code/Advent of Code/Day 1/Day1.swift
+++ Advent of Code/Advent of Code/Day 1/Day1.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Day1 {
-    static func puzzle1(_ bundle: Bundle) {
+    func puzzle1(_ bundle: Bundle) {
         if let path = bundle.path(forResource: "day1_1", ofType: "txt") {
             do {
                 let data = try String(contentsOfFile: path, encoding: .utf8)
@@ -39,7 +39,44 @@ struct Day1 {
         }
     }
     
-    static func puzzle2(_ bundle: Bundle) {
-        
+    func puzzle2(_ bundle: Bundle) {
+        if let path = bundle.path(forResource: "day1_1", ofType: "txt") {
+            do {
+                let data = try String(contentsOfFile: path, encoding: .utf8)
+                
+                let depths = data
+                    .components(separatedBy: .newlines)
+                    .filter { !$0.isEmpty }
+                    .map { Int($0)! }
+                
+                var sums = [Int]()
+                for (index, _) in depths.enumerated() {
+                    if index == depths.count - 2 {
+                        break
+                    }
+                    
+                    sums.append(depths[index] + depths[index+1] + depths[index+2])
+                }
+                
+                var prev = 0
+                var increase = 0
+                
+                sums.forEach {
+                        if $0 > prev {
+                            increase+=1
+                        }
+                        
+                        prev = $0
+                    }
+                
+                // subtract 1 to account for initial read
+                print(increase-1)
+                
+            } catch {
+                print(error)
+            }
+        } else {
+            print("no file found")
+        }
     }
 }
